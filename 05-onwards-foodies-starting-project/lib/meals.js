47be188c16c457bb81b9f7647a47707f36bd5883
codegorrilla <code.gorrilla@gmail.com>
2025-08-05 14:36:35 +0530

    learned how to load and render meal details via dynamic routes and how to handle form data with building an image picker input field with image preview with server action

--- 05-onwards-foodies-starting-project/lib/meals.js
+++ 05-onwards-foodies-starting-project/lib/meals.js
@@ -1,8 +1,14 @@
-import sql from 'better-sqlite3';
+import sql from 'better-sqlite3'; // protect from sql injection attack if I use dynamic values inside react statements
 
 const db = sql('meals.db');
 
 export async function getMeals() {
-	await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay for demonstration purposes
+	await new Promise((resolve) => setTimeout(resolve, 5000)); // Simulate a delay for demonstration purposes
+
+	//throw new Error('Loading meals failed');
 	return db.prepare('SELECT * FROM meals').all();
+}
+
+export function getMeal(slug) {
+	return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
